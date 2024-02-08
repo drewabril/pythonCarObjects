383ace6de72a3121f96013baa7787bfa3d8daa4a
Drew <drewabril@gmail.com>
2024-02-08 13:00:47 -0500

    working

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-#once i figure out git start this!
-#needs more edits
+from car import Car
+
+car_1 = Car("Chevrolet","Corvette",2024,"blue")
+
+print(car_1.make)
+print(car_1.model)
+print(car_1.year)
+print(car_1.color)
+
+car_1.drive()
